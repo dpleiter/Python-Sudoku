efecefa906b6b7121d0b48fc7e44fb2c939d3ee6
dpleiter <dpleiter@gmail.com>
2021-06-20 20:46:45 +1000

    grid validation complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 
 game_to_solve = 'easy-std-44-01'
 
@@ -8,35 +9,43 @@ class SudokuGrid:
         self.grid = numpy.zeros(shape=(size, size))
     
     def validate(self) -> bool:
-        print(self.grid)
-
-        checker = set()
-
         # Check rows
-        for a in range(self.size):
-            for b in range(self.size):
-                val = self.grid[a, b]
-
-                if  val != 0:
-                    if val in checker:
-                        raise IOError("Duplicate values")
-                    else:
-                        checker.add(val)
-
-            checker.clear()
-
+        for row_num in range(self.size):
+            if contains_duplicates(self.grid[row_num]):
+                return False
+        
         # Check columns
-
-
-
+        for col_num in range(self.size):
+            if contains_duplicates(self.grid[:,col_num]):
+                return False
 
         # Check boxes
+        for box_num in range(self.size):
+            box_size = int(math.sqrt(self.size))
 
+            rows = (box_size * int(box_num / box_size), box_size * (1 + int(box_num / box_size)))
+            cols = ((box_num % box_size) * box_size, (box_num % box_size + 1) * box_size)
 
+            box = self.grid[rows[0] : rows[1], cols[0] : cols[1]].flatten()
 
-
+            if (contains_duplicates(box)):
+                return False
 
         return True
+
+def contains_duplicates(arr) -> bool:
+    checker = set()
+
+    for x in arr.flatten():
+        if x == 0:
+            continue
+        
+        if x in checker:
+            return True
+        else:
+            checker.add(x)
+
+    return False
 
 with open('games/' + game_to_solve + '.in') as game:
     line = game.readline() # Grid size
@@ -51,4 +60,9 @@ with open('games/' + game_to_solve + '.in') as game:
 
         grid.grid[int(coords[0]), int(coords[1])] = int(cell_info[1])
 
-grid.validate()
+    print(grid.grid)
+
+if grid.validate():
+    print("All good")
+else:
+    print("Invalid Grid")
